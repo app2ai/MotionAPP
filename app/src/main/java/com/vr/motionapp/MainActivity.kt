42023ee3bbaeb1798a83784d8f94f680b0b72929
VishalR <vishal.r@altius.cc>
2020-10-01 23:27:23 +0530

    initial commit

---
+++ app/src/main/java/com/vr/motionapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.vr.motionapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun motion1Clicked(v:View){
+        startActivity(Intent(this, Motion1Activity::class.java))
+    }
+
+    fun motion2Clicked(v:View){
+        startActivity(Intent(this, Motion2Activity::class.java))
+    }
+
+    fun motion3Clicked(v:View){
+        startActivity(Intent(this, Motion3Activity::class.java))
+    }
+
+    fun motion4Clicked(v:View){
+        startActivity(Intent(this, Motion4Activity::class.java))
+    }
+}
---
+++ app/src/main/java/com/vr/motionapp/Motion3Activity.kt
@@ -0,0 +1,12 @@
+package com.vr.motionapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Motion3Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_motion3)
+    }
+}
