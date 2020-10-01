c6bcdbc5c6f301fdee2f21f0b594ae44002ab8db
VishalR <vishal.r@altius.cc>
2020-10-01 23:44:35 +0530

    initial commit 2

---
+++ app/src/main/java/com/vr/motionapp/MotionScrollingActivity.kt
@@ -0,0 +1,19 @@
+package com.vr.motionapp
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_motion_scrolling.*
+
+class MotionScrollingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_motion_scrolling)
+        setSupportActionBar(toolbar)
+        /*fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }*/
+    }
+}
