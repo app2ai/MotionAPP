2255f944bf367a655f0345245449468637665e93
VishalR <vishal.r@altius.cc>
2020-10-10 22:39:46 +0530

    side view motion layout

--- app/src/main/java/com/vr/motionapp/MotionDrawerActivity.kt
+++ app/src/main/java/com/vr/motionapp/MotionDrawerActivity.kt
@@ -2,6 +2,10 @@ package com.vr.motionapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.vr.motionapp.ui.DrawerContent
 
 class MotionDrawerActivity : AppCompatActivity() {
 
