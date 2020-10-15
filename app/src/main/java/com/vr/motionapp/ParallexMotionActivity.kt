65d0df3694096e5a832508ce103805aa748903a2
VishalR <vishal.r@altius.cc>
2020-10-15 23:33:53 +0530

    parrallex motion added with pager

--- app/src/main/java/com/vr/motionapp/ParallexMotionActivity.kt
+++ app/src/main/java/com/vr/motionapp/ParallexMotionActivity.kt
@@ -2,11 +2,32 @@ package com.vr.motionapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.vr.motionapp.ui.ViewPagerAdapter
+import com.vr.motionapp.ui.ViewpagerHeader
+import kotlinx.android.synthetic.main.activity_parallex_motion.*
 
 class ParallexMotionActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_parallex_motion)
+        val layout = R.layout.activity_parallex_motion
+        setContentView(layout)
+
+        val viewPagerHeader = findViewById<ViewpagerHeader>(R.id.motionLayout)
+        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
+
+        viewPagerAdapter.addPage("Page A", R.layout.page_1)
+        viewPagerAdapter.addPage("Page B", R.layout.page_2)
+        viewPagerAdapter.addPage("Page C", R.layout.page_3)
+
+        pager.adapter = viewPagerAdapter
+        tabs.setupWithViewPager(pager)
+
+        if (viewPagerHeader != null){
+            pager.addOnPageChangeListener(viewPagerHeader)
+        }
+
+        viewPagerHeader.setDebugMode(MotionLayout.DEBUG_SHOW_PATH)
     }
 }
---
+++ app/src/main/java/com/vr/motionapp/ui/Page.kt
@@ -0,0 +1,26 @@
+package com.vr.motionapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class Page : Fragment() {
+    private var layoutId = 0
+
+    override fun setArguments(args: Bundle?) {
+        super.setArguments(args)
+        if (args != null){
+            layoutId = args.getInt("layout")
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(layoutId, container, false)
+    }
+}
